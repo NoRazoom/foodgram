5776e48e5942a83c6625b9ff17ff318883adf54d
NoRazoom <kakovtoktoto@yandex.ru>
2026-04-18 16:35:59 +0300

    пробы и ошибки

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -124,7 +124,7 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
     def validate_cooking_time(self, value):
         if value is None or value == "":
             raise serializers.ValidationError("Время приготовления обязательно!")
-        
+
         if isinstance(value, str):
             try:
                 value = int(value)
