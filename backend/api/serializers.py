ebc47ceac8a95a391c3a9bbfbe17d931c08631e9
NoRazoom <kakovtoktoto@yandex.ru>
2026-04-18 13:55:56 +0300

    откат 3

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -88,11 +88,6 @@ class RecipeIngredientCreateSerializer(serializers.ModelSerializer):
         model = models.RecipeIngredient
         fields = ('id', 'amount')
 
-    '''def validate_amount(self, value):
-        value = int(value)
-        if value < models.MIN_AMOUNT or value > models.MAX_AMOUNT:
-            raise serializers.ValidationError("Недопустимое значение!")'''
-
 
 class RecipeCreateSerializer(serializers.ModelSerializer):
     """
@@ -120,11 +115,6 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
         model = models.Recipe
         fields = ('ingredients', 'tags', 'image',
                   'name', 'text', 'cooking_time', 'author')
-
-    '''def validate_cooking_time(self, value):
-        value = int(value)
-        if value < models.MIN_COOKING_TIME or value > models.MAX_COOCKING_TIME:
-            raise serializers.ValidationError("Недопустимое значение!")'''
 
     def create(self, validated_data):
         ingredients = validated_data.pop('ingredients', [])
